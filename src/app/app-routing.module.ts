4550b43d55d295ed841b5e811ff88f57b030b8bb
Vanilson <vanilsonfire123@gmail.com>
2023-02-21 12:15:48 -0300

    update styles and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes , RouterModule } from '@angular/router';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'produtos', pathMatch: 'full'},
-  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
+  { path: '', redirectTo: 'proway-computers/produtos', pathMatch: 'full'},
+  { path: 'proway-computers', redirectTo: 'proway-computers/produtos', pathMatch: 'full'},
+  { path: 'proway-computers/produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  { path: 'proway-computers/contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
   { path: '**', component: NaoEncontradoComponent }
 ]
 
